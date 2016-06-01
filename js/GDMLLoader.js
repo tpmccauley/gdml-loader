ae80a3eae8fcd5e6109ea49e27138cd64d96d0ee
tpmccauley <tpmccauley@gmail.com>
2016-06-01 16:32:40 +0200

    1st working iteration
    * draw a hollow cylinder with extrusion
    
    * parse volumes and physvols
    
    * make meshes and add to scene
    
    * mod xml file to change ecal endcap position

--- js/GDMLLoader.js
+++ js/GDMLLoader.js
@@ -10,61 +10,167 @@
 
  THREE.GDMLLoader.prototype = {
 
-   constructor: THREE.GDMLLoader,
+  constructor: THREE.GDMLLoader,
 
-   load: function ( url, onLoad, onProgress, onError ) {
-      var scope = this;
+  group: new THREE.Group(),
+  geometries: {},
+  refs: {},
+  meshes: [],
 
-      var loader = new THREE.XHRLoader();
-      loader.setPath( this.path );
-      loader.load( url, function ( text ) {
+  load: function ( url, onLoad, onProgress, onError ) {
+    var scope = this;
 
-        onLoad( scope.parse( text ) );
+    var loader = new THREE.XHRLoader();
+    loader.setPath( this.path );
+    loader.load( url, function ( text ) {
 
-      }, onProgress, onError );
-	 },
+      onLoad( scope.parse( text ) );
 
-   parse: function ( text ) {
-      GDML = new DOMParser().parseFromString( text, 'text/xml' );
+    }, onProgress, onError );
+  },
 
-      var solids = this.parseSolids()[0].childNodes;
-      var name = '';
-      var container = new THREE.Group();
+  parse: function ( text ) {
+    GDML = new DOMParser().parseFromString( text, 'text/xml' );
 
-      var geometries = {};
-      var materials = {};
-      var meshes = {};
+    this.parseSolids();
+    this.parseVolumes();
+    this.parsePhysVols();
 
-      for ( var i = 0; i < solids.length; i++ ) {
-        var type = solids[i].nodeName;
+    return this.group;
+  },
 
-        if ( type === 'box' ) {
+  parseSolids: function() {
 
-          name = solids[i].attributes.name;
-          var x = solids[i].attributes.x;
-          var y = solids[i].attributes.y;
-          var z = solids[i].attributes.z;
-        }
+    var elements = GDML.querySelectorAll('solids');
+    var solids = elements[0].childNodes;
 
-        if ( type === 'tube' ) {
+    for ( var i = 0; i < solids.length; i++ ) {
 
-          name = solids[i].attributes.name;
-          var rmin = solids[i].attributes.rmin;
-          var rmax = solids[i].attributes.rmax;
-          var z = solids[i].attributes.z;
-          var startphi = solids[i].attributes.startphi;
-          var deltaphi = solids[i].attributes.deltaphi;
-        }
+      var type = solids[i].nodeName;
+      var solid = solids[i];
+
+      if ( type === 'box' ) {
+
+        name = solid.getAttribute('name');
+        var x = solid.getAttribute('x') / 1e4;
+        var y = solid.getAttribute('y') / 1e4;
+        var z = solid.getAttribute('z') / 1e4;
+
       }
 
-      return geometries;
-   },
+      if ( type === 'tube' ) {
 
-   parseSolids: function() {
-     var elements = GDML.querySelectorAll('solids');
-     return elements;
-   }
+        var aunit = solid.getAttribute('aunit');
+        var lunit = solid.getAttribute('lunit');
 
+        name = solid.getAttribute('name');
+        var rmin = solid.getAttribute('rmin') / 1e4;
+        var rmax = solid.getAttribute('rmax') / 1e4;
+        var z = solid.getAttribute('z') / 1e4;
+        var startphi = solid.getAttribute('startphi') * Math.PI/180.0;
+        var deltaphi = solid.getAttribute('deltaphi') * Math.PI/180.0;
 
+        var shape = new THREE.Shape();
+        // x,y, radius, startAngle, endAngle, clockwise, rotation
+        shape.absarc(0, 0, rmax, startphi, deltaphi, false);
 
- };
+        var hole = new THREE.Path();
+        hole.absarc(0, 0, rmin, startphi, deltaphi, true);
+        shape.holes.push(hole);
+
+        var extrudeSettings = {
+          amount : z,
+          steps : 1,
+          bevelEnabled: false,
+          curveSegments: 24
+        };
+
+        var geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+        geometry.center();
+        this.geometries[name] = geometry;
+
+      }
+    }
+  },
+
+  parseVolumes: function() {
+    var volumes = GDML.querySelectorAll('volume');
+
+    for ( var i = 0; i < volumes.length; i++ ) {
+
+      var name = volumes[i].getAttribute('name');
+      var solidrefs = volumes[i].childNodes;
+
+      for ( var j = 0; j < solidrefs.length; j++ ) {
+
+        var type = solidrefs[j].nodeName;
+
+        if ( type === 'solidref' ) {
+          var solidref = solidrefs[j].getAttribute('ref');
+          this.refs[name] = solidref;
+        }
+      }
+    }
+  },
+
+  parsePhysVols: function() {
+    var physvols = GDML.querySelectorAll('physvol');
+
+    for ( var i = 0; i < physvols.length; i++ ) {
+
+      var name = physvols[i].getAttribute('name');
+      var children = physvols[i].childNodes;
+      var volumeref = '';
+
+      var position = new THREE.Vector3(0,0,0);
+      var rotation = new THREE.Vector3(0,0,0);
+
+      var geometry;
+      var material = new THREE.MeshBasicMaterial({color:Math.random()*0xffffff, transparent:true, opacity:0.5});
+
+      for ( var j = 0; j < children.length; j++ ) {
+
+        var type = children[j].nodeName;
+
+        if ( type === 'volumeref' ) {
+
+          var volumeref = children[j].getAttribute('ref');
+          geometry = this.geometries[this.refs[volumeref]];
+        }
+
+        if ( type === 'position' ) {
+
+          var x = children[j].getAttribute('x') / 1e4;
+          var y = children[j].getAttribute('y') / 1e4;
+          var z = children[j].getAttribute('z') / 1e4;
+
+          position.set(x,y,z);
+        }
+
+        if ( type === 'rotation' ) {
+
+          var x = children[j].getAttribute('x') * Math.PI/180.0;
+          var y = children[j].getAttribute('y') * Math.PI/180.0;
+          var z = children[j].getAttribute('z') * Math.PI/180.0;
+
+          rotation.set(x,y,z);
+        }
+
+      }
+
+      var mesh = new THREE.Mesh(geometry, material);
+      mesh.name = name;
+      mesh.visible = true;
+
+      mesh.position.set(position.x, position.y, position.z);
+      mesh.rotation.set(rotation.x, rotation.y, rotation.z);
+
+      console.log(name, position);
+
+      this.group.add(mesh);
+
+    }
+
+  }
+
+};
